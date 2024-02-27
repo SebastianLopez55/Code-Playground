4e7ff8872b05c3a5ab8aaa93219b020b93bb27e9
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-02-27 00:14:30 -0500

    feat: implement max_product_palindromes

--- Palindrome_Subsequence/max_product_two_palindromic_subsequence.py
+++ Palindrome_Subsequence/max_product_two_palindromic_subsequence.py
@@ -1,18 +1,25 @@
 # 2002. Maximum Product of the Length of Two Palindromic Subsequences
+def is_palindrome(s: str) -> bool:
+    L, R = 0, len(s) - 1
+    while L < R:
+        if s[L] != s[R]:
+            return False
+        L += 1
+        R -= 1
+    return True
 
 
 def maxProduct(s: str) -> int:
     N, pali = len(s), {}  # bitmask : length
 
-    iterable = range(1, 1 << N)
-
-    for mask in iterable:  # 1 << N == 2 ** N
+    # Generating every possible subsequence of the string
+    for mask in range(1, 1 << N):  # 1 << N == 2 ** N
         subseq = ""
         for i in range(N):
+            # Checking first character against LSB of mask.
             if mask & (1 << i):
-                curr = s[i]
-                subseq += curr
-        if subseq == subseq[::-1]:
+                subseq += s[i]
+        if is_palindrome(subseq):
             pali[mask] = len(subseq)
 
     res = 0
@@ -25,9 +32,11 @@ def maxProduct(s: str) -> int:
 
 
 # Driver
-
 s = "leetcodecom"
 print(maxProduct(s))
 
-s = "bb"
+s = "abc"
 print(maxProduct(s))
+
+# O(4^n) time
+# O(2^n) space
