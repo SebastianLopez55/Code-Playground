8e39fd8a6dc742faa4b89d6c196ef06f27e38f6c
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-03-04 15:05:54 -0500

    feat: LinkedList driver

--- LinkedList/palindrome_LinkedList.py
+++ LinkedList/palindrome_LinkedList.py
@@ -1,3 +1,9 @@
 from LinkedList import LinkedList, ListNode
 
 my_LinkedList = LinkedList()
+
+my_LinkedList.append(5)
+my_LinkedList.append(3)
+my_LinkedList.append(1)
+
+my_LinkedList.print_list()
