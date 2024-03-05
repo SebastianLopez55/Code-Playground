414bc3d9b0c5e74fe3cabdf0d9b91bc01435b732
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-03-05 14:42:17 -0500

    feat: Update linked list

--- LinkedList/palindrome_LinkedList.py
+++ LinkedList/palindrome_LinkedList.py
@@ -5,5 +5,7 @@ my_LinkedList = LinkedList()
 my_LinkedList.append(5)
 my_LinkedList.append(3)
 my_LinkedList.append(1)
+my_LinkedList.append(1)
+my_LinkedList.append(3)
 
 my_LinkedList.print_list()
