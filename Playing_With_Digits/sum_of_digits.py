185fcdd492d06221ebdf040f6270d67e8d61f195
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-03-10 19:32:37 -0400

    feat: implement sum digits of num

--- Playing_With_Digits/sum_of_digits.py
+++ Playing_With_Digits/sum_of_digits.py
@@ -1,3 +1,14 @@
+"""
+sum_of_digits() complexity analysis:
+
+Time Complexity: the time complexity of the algorithm depends on how many digits the number has.
+    - The key operation is dividing the given integer by floor of 10. 
+    - Thus, the time complexity is given by the number of operations which is in turn given my the number
+    of times we need to divide the given number by floor of 10 till we arrive to zero digits. 
+
+"""
+
+
 def sum_of_digits(n):
     digit_sum = 0
     while n > 0:
@@ -6,6 +17,9 @@ def sum_of_digits(n):
         n = n // 10
     return digit_sum
 
+
+# O(log n) time
+# O(1) space
 
 n = 1234
 print(sum_of_digits(n))
