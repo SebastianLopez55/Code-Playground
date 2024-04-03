dd271e5d47195a029db2c4c5784071d96468f8ca
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-04-03 15:51:25 -0400

    refactor: kSum base case structure.

--- Two_Pointers/kSum.py
+++ Two_Pointers/kSum.py
@@ -2,32 +2,39 @@ def kSum(k, nums, target):
     nums.sort()
     q_plets, curr_kSum = [], []
 
-    def kSum(k, start, target):
-        if k != 2:
-            for i in range(start, len(nums) - k + 1):
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
-                curr_kSum.append(nums[i])
-                kSum(k - 1, i + 1, target - nums[i])
-
-                curr_kSum.pop()
+    def kSum_helper(k, start, target):
+        # New structure: Check for the base case at the beginning
+        if k == 2:  # Base case: two sum II
+            l, r = start, len(nums) - 1
+            while l < r:
+                current_sum = nums[l] + nums[r]
+                if current_sum < target:
+                    l += 1
+                elif current_sum > target:
+                    r -= 1
+                else:
+                    q_plets.append(curr_kSum + [nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
             return
 
-        # base case, two sum II
-        l, r = start, len(nums) - 1
-        while l < r:
-            if nums[l] + nums[r] < target:
-                l += 1
-            elif nums[l] + nums[r] > target:
-                r -= 1
-            else:
-                q_plets.append(curr_kSum + [nums[l], nums[r]])
-                l += 1
-                while l < r and nums[l] == nums[l - 1]:
-                    l += 1
+        # Recursive case for k > 2
+        for i in range(start, len(nums) - k + 1):
+            if i > start and nums[i] == nums[i - 1]:  # Skip duplicates
+                continue
+            curr_kSum.append(nums[i])
+            kSum_helper(k - 1, i + 1, target - nums[i])
+            curr_kSum.pop()
 
-    kSum(k, 0, target)
+    kSum_helper(k, 0, target)
     return q_plets
+
+
+# Example usage
+nums = [1, 0, -1, 0, -2, 2]
+target = 0
+print(kSum(4, nums, target))
 
 
 nums = [1, 0, -1, 0, -2, 2]
@@ -38,7 +45,7 @@ target = 0
 
 # assert kSum(1, nums, target) == [], "failed 1Sum test one."
 
-assert kSum(2, nums, target) == [[-2, 2], [-1, 1], [0, 0]], "failed 2Sum test one."
+# assert kSum(2, nums, target) == [[-2, 2], [-1, 1], [0, 0]], "failed 2Sum test one."
 
 assert kSum(4, nums, target) == [
     [-2, -1, 1, 2],
