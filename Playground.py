8eae37f3dd0894d1e84d19e6aa74beabcf239d98
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-04-15 20:58:09 -0400

    feat: playground

--- Playground.py
+++ Playground.py
@@ -1,10 +1,19 @@
-graph = {0: [1, 2, 3], 1: [4], 2: [], 3: [], 4: [5], 5: [6], 6: []}
+n = 5
+visited = [False] * n
 
-print(len(graph.keys()))
-print(len(graph))
+print(any(visited))
+
+for i in range(n):
+    if i % 2 == 0:
+        visited[i] = True
 
 
-l = [1, 2, 3, 4, 5]
+print(any(visited))
 
-for i, num in enumerate(l, 0):
-    print(i, num)
+
+print(visited)
+
+gen = (not i for i in visited)
+
+for el in gen:
+    print(el)
