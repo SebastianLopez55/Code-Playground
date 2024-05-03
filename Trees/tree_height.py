04fc4d896eaf5d71097ac7fcb7e61ea817e1c3e7
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-05-03 09:54:04 -0400

    feat: tree height.

--- Trees/tree_height.py
+++ Trees/tree_height.py
@@ -12,6 +12,13 @@ def maxDepth(root: TreeNode) -> int:
     return 1 + max(maxDepth(root.left), maxDepth(root.right))
 
 
+def treeHeight(root: TreeNode) -> int:
+    # Recursive DFS
+    if root is None:
+        return -1
+    return 1 + max(treeHeight(root.left), treeHeight(root.right))
+
+
 # Creating nodes
 node1 = TreeNode(1)
 node2 = TreeNode(2)
@@ -19,8 +26,10 @@ node3 = TreeNode(3)
 
 # Connecting nodes
 node1.left = node2
-node1.left = node3
+node1.right = node3
 
 # Now pass the root of the tree (node1) to the maxDepth function
 depth = maxDepth(node1)
+height = treeHeight(node1)
 print("The maximum depth of the binary tree is:", depth)
+print("The height of the binary tree is:", height)
