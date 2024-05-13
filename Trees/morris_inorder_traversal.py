f4aef33383c72f09dbf37fe1e907da1356bcbe6d
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-05-13 14:24:56 -0400

    feat: implement inorder morris traversal.

--- Trees/morris_inorder_traversal.py
+++ Trees/morris_inorder_traversal.py
@@ -21,5 +21,35 @@ def inorder_dfs(root):
     print(root.val)
     inorder_dfs(root.right)
 
+    # O(n) time.
+    # O(n) space.
 
-inorder_dfs(tree)
+
+# Morris Traversal: Inorder constant space tree traversal.
+def morris_inorder(root):
+    current = root
+    while current:
+        if current.left is None:
+            print(current.val)
+            current = current.right
+        else:
+            # Find inorder predecessor of current.
+            pre = current.left
+            while pre.right is not None and pre.right is not current:
+                pre = pre.right
+
+            if pre.right is None:
+                # Make current as the right child of its inorder predecessor.
+                pre.right = current
+                current = current.left
+            else:
+                # Revert changes made in tree.
+                pre.right = None
+                print(current.val)
+                current = current.right
+
+    # O(n) time.
+    # O(1) space.
+
+
+morris_inorder(tree)
