28bc06c23db6e57b320f3439ae140ecf90462a2b
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-05-19 10:28:43 -0400

    PG

--- Playground.py
+++ Playground.py
@@ -1,4 +1,6 @@
 num = 239
+lst = [2, 3, 9]
+lst1 = []
 
 n1 = num % 100
 print(n1)  # 39
@@ -13,3 +15,10 @@ n4 = num // 10
 print(n4)  # 23
 
 print(str(num)[0])
+
+
+for i in range(len(lst)):
+    lst1.append(int(str(num)[i]))
+
+
+print(lst1)
