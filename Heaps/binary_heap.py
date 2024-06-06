02a5227c7be5eb064099e97c89169329f75de468
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-06-06 11:44:18 -0400

    refactor: use private attributes.

--- Heaps/binary_heap.py
+++ Heaps/binary_heap.py
@@ -1,93 +1,80 @@
-from operator import index
 from typing import List
 import random
 
 
 class MinHeap:
     def __init__(self) -> None:
-        self.size = 0
-        self.items = []
+        self._size = 0
+        self._items = []
 
-    def print_heap(self):
-        print(f"Current state of the Min Heap: {self.items}")
+    def print_heap(self) -> None:
+        print(f"Current state of the Min Heap: {self._items}")
 
-    def is_empty(self):
-        return self.size == 0
+    def is_empty(self) -> bool:
+        return self._size == 0
 
-    def get_left_child_idx(self, parent_idx):
+    def get_left_child_idx(self, parent_idx: int) -> int:
         left_child_idx = 2 * parent_idx + 1
-        if left_child_idx >= self.size:
+        if left_child_idx >= self._size:
             raise IndexError("Left child index is out of bounds")
         return left_child_idx
 
-    def get_right_child_idx(self, parent_idx):
+    def get_right_child_idx(self, parent_idx: int) -> int:
         right_child_idx = 2 * parent_idx + 2
-        if right_child_idx >= self.size:
+        if right_child_idx >= self._size:
             raise IndexError("Right child index is out of bounds")
         return right_child_idx
 
-    def get_parent_idx(self, child_idx):
-        if child_idx <= 0 or child_idx >= self.size:
+    def get_parent_idx(self, child_idx: int) -> int:
+        if child_idx <= 0 or child_idx >= self._size:
             raise IndexError("Child index is out of bounds")
         parent_idx = (child_idx - 1) // 2
         return parent_idx
 
-    def has_left_child(self, idx):
-        try:
-            self.get_left_child_idx(idx)
-            return True
-        except IndexError:
-            return False
+    def has_left_child(self, idx: int) -> bool:
+        return 2 * idx + 1 < self._size
 
-    def has_right_child(self, idx):
-        try:
-            self.get_right_child_idx(idx)
-            return True
-        except IndexError:
-            return False
+    def has_right_child(self, idx: int) -> bool:
+        return 2 * idx + 2 < self._size
 
-    def has_parent(self, idx):
-        try:
-            self.get_parent_idx(idx)
-            return True
-        except IndexError:
-            return False
+    def has_parent(self, idx: int) -> bool:
+        return (idx - 1) // 2 >= 0
 
-    def left_child(self, idx):
-        return self.items[self.get_left_child_idx(idx)]
+    def left_child(self, idx: int) -> int:
+        return self._items[self.get_left_child_idx(idx)]
 
-    def right_child(self, idx):
-        return self.items[self.get_right_child_idx(idx)]
+    def right_child(self, idx: int) -> int:
+        return self._items[self.get_right_child_idx(idx)]
 
-    def parent(self, idx):
-        return self.items[self.get_parent_idx(idx)]
+    def parent(self, idx: int) -> int:
+        return self._items[self.get_parent_idx(idx)]
 
-    def _swap(self, idx_one, idx_two):
-        self.items[idx_one], self.items[idx_two] = (
-            self.items[idx_two],
-            self.items[idx_one],
+    def _swap(self, idx_one: int, idx_two: int) -> None:
+        self._items[idx_one], self._items[idx_two] = (
+            self._items[idx_two],
+            self._items[idx_one],
         )
 
-    def get_min(self):
-        if self.size == 0:
-            raise LookupError()
-        return self.items[0]
+    def get_min(self) -> int:
+        if self._size == 0:
+            raise LookupError("Heap is empty")
+        return self._items[0]
 
-    def extract_min(self):
-        if self.size == 0:
-            raise LookupError()
-        min_item = self.items[0]
-        self.items[0] = self.items[self.size - 1]
-        self.size -= 1
+    def extract_min(self) -> int:
+        if self._size == 0:
+            raise LookupError("Heap is empty")
+        min_item = self._items[0]
+        self._items[0] = self._items[self._size - 1]
+        self._size -= 1
         self._bubble_down()
         return min_item
 
-    def insert(self, item):
-        self.items.append(item)
-        self.size += 1
+    def insert(self, item: int) -> None:
+        self._items.append(item)
+        self._size += 1
         self._bubble_up()
 
-    def _bubble_down(self):
+    def _bubble_down(self) -> None:
         item_idx = 0
         while self.has_left_child(item_idx):
             smaller_child_idx = self.get_left_child_idx(item_idx)
@@ -96,16 +83,16 @@ class MinHeap:
             ) < self.left_child(item_idx):
                 smaller_child_idx = self.get_right_child_idx(item_idx)
 
-            if self.items[item_idx] < self.items[smaller_child_idx]:
+            if self._items[item_idx] < self._items[smaller_child_idx]:
                 break
             else:
                 self._swap(item_idx, smaller_child_idx)
             item_idx = smaller_child_idx
 
-    def _bubble_up(self):
-        item_idx = self.size - 1
+    def _bubble_up(self) -> None:
+        item_idx = self._size - 1
         while (
-            self.has_parent(item_idx) and self.parent(item_idx) > self.items[item_idx]
+            self.has_parent(item_idx) and self.parent(item_idx) > self._items[item_idx]
         ):
             self._swap(self.get_parent_idx(item_idx), item_idx)
             item_idx = self.get_parent_idx(item_idx)
@@ -117,4 +104,11 @@ for _ in range(10):
     item = random.randint(1, 20)
     print(f"Item to be added to heap: {item}")
     min_heap.insert(item)
+    min_heap.print_heap()
+
+print()
+
+for _ in range(5):
+    print(f"Item to be extracted from heap: {min_heap.get_min()}")
+    min_heap.extract_min()
     min_heap.print_heap()
