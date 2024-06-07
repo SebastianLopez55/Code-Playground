4da67814a025a42a17d4450a79b87967bd8caf9d
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-06-06 23:20:52 -0400

    PG

--- Playground.py
+++ Playground.py
@@ -1,10 +1,4 @@
-import heapq
+lst = ["1", "1", "2"]
+s = "".join(lst)
 
-ls1 = [1, 234, 3241, 23, 2]
-for i in range(len(ls1)):
-    ls1[i] = -ls1[i]
-heapq.heapify(ls1)
-print(ls1)
-max1, max2 = -heapq.heappop(ls1), -heapq.heappop(ls1)
-print(ls1)
-print(f"This is the first max {max1} and this is the second max {max2}.")
+print(s)
