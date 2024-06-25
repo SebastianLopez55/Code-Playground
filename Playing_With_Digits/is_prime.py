5498d05c9a916e7eb9232c729a37127d35326606
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-06-24 22:23:09 -0400

    feat: is_prime(.)

---
+++ Playing_With_Digits/is_prime.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    """Helper function to determine if n is prime."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+for i in range(-10, 100):
+    if is_prime(i):
+        print(f"Is prime: {i}")
