039102c413c8bc410aaa75b502f613dd85674241
SebastianLopez55 <lopezsebastian856@gmail.com>
2024-07-05 21:19:02 -0400

    feat: implement library.

--- OOP/book_store.py
+++ OOP/book_store.py
@@ -34,14 +34,16 @@ class Library:
         self.book_count = 0
 
     def add_book(self, book, book_id):
-        self.book_collection[book_id] = [book, False]
-        self.book_count += 1
+        if book_id not in self.book_collection:
+            self.book_collection[book_id] = [book, False]
+            self.book_count += 1
+            print(f'Book "{book.book_title}" added to library.')
 
     def is_rented(self, book_id):
         if book_id not in self.book_collection:
             print(f"Book with id {book_id} not in collection.")
 
-        return self.book_collection[id][1]
+        return self.book_collection[book_id][1]
 
     def rent_book(self, book_id):
         if not self.is_rented(book_id):
@@ -53,5 +55,33 @@ class Library:
         print("Book Catalog: ")
         for book_id, book_info in self.book_collection.items():
             print(
-                f"Book with id {book_id} and title {book_info[0].book_title} is available: {book_info[1]}"
+                f'Book with id "{book_id}" and title "{book_info[0].book_title}" is available: {not self.is_rented(book_id)}'
             )
+
+
+# Book 1
+def book_factory(title, pages, content):
+    book = Book(pages, title)
+    for i in range(1, pages + 1):
+        book.write(i, content)
+    return book
+
+
+book1 = book_factory("Harry Potter Summary", 5, "bla bla bla bla")
+book2 = book_factory("Bed Stories", 4, "Sleep story ...")
+
+lib = Library()
+lib.add_book(book1, "A43")
+lib.add_book(book2, "A76")
+
+print(lib.is_rented("A43"))
+print(lib.is_rented("A76"))
+
+lib.book_catalog()
+
+result = lib.rent_book("A43")
+if result:
+    print(result.book_title)
+
+print(lib.is_rented("A43"))
+lib.book_catalog()
